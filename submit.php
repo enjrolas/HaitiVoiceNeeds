72f41e69028783bb1c18e8339d59b5a1c95d1e93
japhy <japhy@li64-162.members.linode.com>
2010-02-13 14:10:30 -0500

    added ron's submit.php

--- submit.php
+++ submit.php
@@ -2,14 +2,31 @@
 require_once("connect.php");
 require_once("string_utils.php");
 
+# This is very crude and should be improved
+define('VALID_URL_REGEX', '/^https?:\/\/.+$/i');
+
+function isValidURL($s) {
+    return preg_match(VALID_URL_REGEX, $s);
+}
+
 $recording_url=$_REQUEST['recording_url'];
 $callback_url=$_REQUEST['callback_url'];
 $source=$_REQUEST['source'];
 
-$recording_url=sanitizeString($recording_url);
-$callback_url=sanitizeString($callback_url);
+$recording_url=sanitizeString(trim($recording_url));
+$callback_url=sanitizeString(trim($callback_url));
 $source=sanitizeString($source);
 
+header("Content-type: text/plain");
+if (!isValidURL($recording_url)) {
+    echo("not ok: recording_url '$recording_url'\n");
+    exit;
+} 
+if (!empty($callback_url) && !isValidURL($callback_url)) {
+    echo("not ok: callback_url '$callback_url'\n");
+    exit;
+} 
 $query="insert into recordings(url, callback_url, source) values('$recording_url', '$callback_url', '$source')";
 mysql_query($query);
+echo "ok\n";
 ?>
