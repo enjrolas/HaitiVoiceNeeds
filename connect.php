0ae9db87439067a59692739d62726be839d1daa4
japhy <japhy@li64-162.members.linode.com>
2010-02-13 14:11:32 -0500

    added connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+/*this file has the connection variables for the database.  Whenever it's included
+  it opens up a database connection in that script*/
+
+$username="haiti";
+$password="haiti";
+$host="localhost";
+$database="haiti";
+
+$connection = mysql_connect($host, $username, $password) or die ('Error connecting to mysql');
+mysql_select_db($database, $connection);
+?>
