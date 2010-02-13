9d691738a59c0843aa59baf9420f28edc290b622
Andrew Bialecki <andrewbialecki@bialecki.local>
2010-02-13 15:20:24 -0500

    More formatting.

--- header.php
+++ header.php
@@ -1,16 +1,14 @@
 <?php
 
-function useJqueryUICode()
-{
-  echo "<script type='text/javascript' src='js/jquery.js'></script>\n";
-  echo "<script type='text/javascript' src='js/input.js'></script>\n"; 
-  echo "<script type='text/javascript' src='js/jQuery.jPlayer.0.2.5/jquery.jplayer.js'></script>\n"; 
+function useJqueryUICode() {
+  echo "<script type='text/javascript' src='js/jquery.js'></script>";
+  echo "<script type='text/javascript' src='js/input.js'></script>"; 
+  echo "<script type='text/javascript' src='js/jQuery.jPlayer.0.2.5/jquery.jplayer.js'></script>"; 
 }
 
-function useCSS()
-{
-  echo "<LINK href='css/style.css' rel='stylesheet' type='text/css'>\n";
-  echo "<LINK href='js/jQuery.jPlayer.0.2.5/jquery.jplayer.css' rel='stylesheet' type='text/css'>\n";
+function useCSS() {
+  echo "<LINK href='css/style.css' rel='stylesheet' type='text/css'>";
+  echo "<LINK href='js/jQuery.jPlayer.0.2.5/jquery.jplayer.css' rel='stylesheet' type='text/css'>";
 }
 
 ?>
